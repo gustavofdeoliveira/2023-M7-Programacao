67846a55393a9a654ed8cddf023e61189643b827
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2023-08-13 18:22:59 -0300

    feat: add comments

--- Semana-01/app.js
+++ Semana-01/app.js
@@ -1,10 +1,13 @@
+//Initialize express router
 const express = require('express'); 
 const app = express();
 
+//Set default API response
 const hostname = '0.0.0.0';
 const port = 80;
+//Import the frontend
 app.use(express.static("./frontend/"));
-
+// start the server
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
