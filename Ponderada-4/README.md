d610daab48c3d4b9299b6df6886370fa923757f3
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2023-10-01 15:58:15 -0300

    feat: ponderada finalizada

--- Ponderada-4/README.md
+++ Ponderada-4/README.md
@@ -1,6 +1,7 @@
 # Podenrada 4 - Intefacegráfica com Modelo
 
 ## Tecnologias utilizadas
+
 1. Python
 2. HTML
 3. CSS
@@ -10,8 +11,8 @@
 7. PostgreSQL
 
 ## Variaveis de Ambiente
-`DATABASE_URL="postgresql://admin123:admin123@ponderada-4.cbgyqrypqeeb.us-east-1.rds.amazonaws.com:5432/postgres"
-SECRET_KEY = 'iuy287e1by87oyn'`
+
+`DATABASE_URL="postgresql://admin123:admin123@ponderada-4.cbgyqrypqeeb.us-east-1.rds.amazonaws.com:5432/postgres" SECRET_KEY = 'iuy287e1by87oyn'`
 
 ## Usuário Teste
 
@@ -20,7 +21,6 @@ SECRET_KEY = 'iuy287e1by87oyn'`
 
 **Mota:** o sistema possui a parte de criação de conta
 
-
 ## Como rodar o projeto
 
 1. Dentro da pasta raiz do projeto, execute o comando `docker compose up` para instalar as dependências do projeto.
@@ -28,6 +28,11 @@ SECRET_KEY = 'iuy287e1by87oyn'`
 
 ## Documentação da API
 
-A documentação da API está disponível em `http://localhost:80/docs`. Lá você pode testar as rotas e ver os modelos de dados por meio do Swagger.
+### Documentação Local
+
+1. A documentação da API está disponível em `http://localhost:80/docs`. Lá você pode testar as rotas e ver os modelos de dados por meio do Swagger.
+
+### Documentação na AWS
+1. A documentação da API está disponível em `http://ec2-3-142-224-4.us-east-2.compute.amazonaws.com/docs`. Lá você pode testar as rotas e ver os modelos de dados por meio do Swagger.
 
 ## Video de demonstração
--- Ponderada-4/frontend/js/dashboard.js
+++ Ponderada-4/frontend/js/dashboard.js
@@ -1,12 +1,11 @@
 window.addEventListener("load", function () {
   if (!localStorage.getItem("token")) {
-    window.location.href = "http://54.175.176.57:80/";
+    window.location.href = "http://54.145.7.217:80/";
   }
   if (localStorage.getItem("token")) {
-
     const token = localStorage.getItem("token");
     // Send the message to the server using Fetch
-    fetch("http://54.175.176.57:3000/user/all", {
+    fetch("http://54.145.7.217:3000/user/all", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -30,7 +29,7 @@ window.addEventListener("load", function () {
       })
       .finally(() => {});
 
-    fetch("http://54.175.176.57:3000/prediction/all", {
+    fetch("http://54.145.7.217:3000/prediction/all", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -62,7 +61,7 @@ window.addEventListener("load", function () {
             }
             this.document.getElementById("predictions").innerHTML += prediction;
             dates.push(response[i]["createdAt"].split(" ", 2)[0]);
-            values.push(response[i]["value"]);        
+            values.push(response[i]["value"]);
           }
           dates = dates.filter(function (este, i) {
             return dates.indexOf(este) === i;
@@ -75,7 +74,7 @@ window.addEventListener("load", function () {
               labels: dates,
               datasets: [
                 {
-                  label: "numebr of predictions per day",
+                  label: "predictions per day",
                   data: values,
                   borderWidth: 1,
                 },
@@ -99,5 +98,133 @@ window.addEventListener("load", function () {
         // Handle the error, e.g., display an error message to the user
       })
       .finally(() => {});
+
+    document.getElementById("logout").addEventListener("click", function (e) {
+      e.preventDefault(); // Prevent the default form submission behavior
+      debugger;
+      localStorage.removeItem("token");
+      window.location.href = "http://54.145.7.217:80/";
+    });
+
+    document
+      .getElementById("new-prediction")
+      .addEventListener("click", function (e) {
+        e.preventDefault(); // Prevent the default form submission behavior
+        document.getElementById("modal").classList.add("absolute", "z-10");
+        document.getElementById("modal").classList.remove("hidden");
+        document.getElementById("content").classList.add("relative", "z-0");
+      });
+
+    document
+      .getElementById("close-modal")
+      .addEventListener("click", function (e) {
+        e.preventDefault(); // Prevent the default form submission behavior
+        document.getElementById("modal").classList.remove("absolute", "z-10");
+        document.getElementById("modal").classList.add("hidden");
+        document.getElementById("content").classList.remove("relative", "z-0");
+        location.reload();
+      });
+
+    document
+      .getElementById("prediction-form")
+      .addEventListener("submit", function (e) {
+        e.preventDefault(); // Prevent the default form submission behavior
+        // Get the user's message from the input field
+        debugger;
+        const subscribers = document.getElementById("subscribers").value;
+        const video_views = document.getElementById("video_views").value;
+        const category = document.getElementById("category").value;
+        const uploads = document.getElementById("uploads").value;
+        const country = document.getElementById("country").value;
+        const channel_type = document.getElementById("channel_type").value;
+        const video_views_rank =
+          document.getElementById("video_views_rank").value;
+        const country_rank = document.getElementById("country_rank").value;
+        const channel_type_rank =
+          document.getElementById("channel_type_rank").value;
+        const video_views_last_30_days = document.getElementById(
+          "video_views_last_30_days"
+        ).value;
+        const subscribers_last_30_days = document.getElementById(
+          "subscribers_last_30_days"
+        ).value;
+
+        const parts = token.split(".");
+
+        if (parts.length === 3) {
+          const payload = JSON.parse(atob(parts[1]));
+
+          user_id = payload.id;
+        } else {
+          console.error("Invalid JWT format");
+        }
+
+        const submitButton = document.getElementById("submit-button");
+        submitButton.disabled = true;
+        submitButton.innerHTML = "Loading...";
+        if (
+          subscribers != "" &&
+          video_views != "" &&
+          category != "" &&
+          uploads != "" &&
+          country != "" &&
+          channel_type != "" &&
+          video_views_rank != "" &&
+          country_rank != "" &&
+          channel_type_rank != "" &&
+          video_views_last_30_days != "" &&
+          subscribers_last_30_days != "" &&
+          user_id != ""
+        ) {
+          // Send the message to the server using Fetch
+          fetch("http://54.145.7.217:3000/prediction/predict", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+              subscribers,
+              video_views,
+              category,
+              uploads,
+              country,
+              channel_type,
+              video_views_rank,
+              country_rank,
+              channel_type_rank,
+              video_views_last_30_days,
+              subscribers_last_30_days,
+              user_id,
+            }),
+          })
+            .then(async (response) => {
+              if (response.ok) {
+                response = await response.json();
+                document.getElementById("result").innerHTML =
+                  '<div class="bg-green-500 text-center mt-4 w-full border-2 text-white py-2 px-2">' +
+                  response.value +
+                  "</div>";
+              } else {
+                submitButton.disabled = false;
+                submitButton.innerHTML = "Submit";
+                throw new Error("Failed to send message");
+              }
+            })
+            .catch((error) => {
+              console.error(error);
+              alert(`Error: ${error.message}`);
+              // Handle the error, e.g., display an error message to the user
+            })
+            .finally(() => {
+              // Remove loading button
+              submitButton.disabled = false;
+              submitButton.innerHTML = "Submit";
+            });
+        } else {
+          document.getElementById("result").innerHTML =
+            '<div class="bg-red-500 text-center mt-4 w-full border-2 text-white py-2 px-2">Please fill all the fields </div>';
+        }
+      });
   }
 });
